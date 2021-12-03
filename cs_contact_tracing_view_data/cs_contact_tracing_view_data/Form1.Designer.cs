a0d1082bea5dcd438c24ea2b1a86ef76bfb4b571
Romwil <romwil.pilapil@gmail.com>
2021-12-04 00:07:28 +0800

    upload contact tracing view data app

---
+++ cs_contact_tracing_view_data/cs_contact_tracing_view_data/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace cs_contact_tracing_view_data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.studentData = new System.Windows.Forms.Label();
+            this.studentDataBox = new System.Windows.Forms.RichTextBox();
+            this.retrieveButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(488, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Menu;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.studentData);
+            this.panel2.Location = new System.Drawing.Point(11, 16);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(464, 68);
+            this.panel2.TabIndex = 1;
+            // 
+            // studentData
+            // 
+            this.studentData.AutoSize = true;
+            this.studentData.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentData.Location = new System.Drawing.Point(3, 12);
+            this.studentData.Name = "studentData";
+            this.studentData.Size = new System.Drawing.Size(185, 31);
+            this.studentData.TabIndex = 2;
+            this.studentData.Text = "Student Data";
+            // 
+            // studentDataBox
+            // 
+            this.studentDataBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.studentDataBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentDataBox.Location = new System.Drawing.Point(12, 130);
+            this.studentDataBox.Name = "studentDataBox";
+            this.studentDataBox.Size = new System.Drawing.Size(488, 249);
+            this.studentDataBox.TabIndex = 2;
+            this.studentDataBox.Text = "Click retrieve ";
+            // 
+            // retrieveButton
+            // 
+            this.retrieveButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.retrieveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.retrieveButton.Location = new System.Drawing.Point(314, 395);
+            this.retrieveButton.Name = "retrieveButton";
+            this.retrieveButton.Size = new System.Drawing.Size(85, 43);
+            this.retrieveButton.TabIndex = 3;
+            this.retrieveButton.Text = "Retrieve";
+            this.retrieveButton.UseVisualStyleBackColor = false;
+            this.retrieveButton.Click += new System.EventHandler(this.retrieveButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.Crimson;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(415, 395);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(85, 43);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(513, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.retrieveButton);
+            this.Controls.Add(this.studentDataBox);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Data";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label studentData;
+        private System.Windows.Forms.RichTextBox studentDataBox;
+        private System.Windows.Forms.Button retrieveButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ cs_contact_tracing_view_data/cs_contact_tracing_view_data/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace cs_contact_tracing_view_data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void retrieveData()
+        {
+            string readStudentFile = @"C:\Users\Rom\Desktop\5TH YR 1ST SEM FILES\OOP\contact-tracing\student-data.txt";
+            string printData = File.ReadAllText(readStudentFile);
+
+            studentDataBox.Text = printData;
+
+        }
+
+        private void retrieveButton_Click(object sender, EventArgs e)
+        {
+            retrieveData();
+            MessageBox.Show("Retrieve successfully");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank you for using");
+            this.Close();
+        }
+    }
+}
